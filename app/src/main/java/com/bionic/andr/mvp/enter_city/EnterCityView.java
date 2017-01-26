2efd22ae69582db2353a0b8f936035d42c656627
Дмитрий Скопинцев < >
2017-01-26 19:22:34 +0200

    Complete

---
+++ app/src/main/java/com/bionic/andr/mvp/enter_city/EnterCityView.java
@@ -0,0 +1,17 @@
+package com.bionic.andr.mvp.enter_city;
+
+import com.bionic.andr.api.data.Weather;
+
+import rx.Observable;
+
+/**  */
+public interface EnterCityView {
+
+    Observable<CharSequence> cityChange();
+    Observable<Void> submit();
+
+    void onValidationCheck(boolean valid);
+    void showProgress(boolean show);
+    void onWeatherLoaded(Weather weather);
+    void onError(int status);
+}
